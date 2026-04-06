c6510c4f5ffffe3bcb0ff12b728b184f79ca71d3
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-06 16:58:45 +0530

    Clean up unnecessary blank lines across JobService, JobRepo, and JobController files for improved readability.

--- src/main/java/com/app/controller/JobController.java
+++ src/main/java/com/app/controller/JobController.java
@@ -16,24 +16,20 @@ public class JobController {
     @Autowired
     private JobService service;
 
-
     @GetMapping({"/", "home"})
     public String home() {
         return "home";
     }
-
 
     @GetMapping("addjob")
     public String addJob() {
         return "addjob";
     }
 
-
     @PostMapping("handleForm")
     public String handleForm(JobPost jobPost) {
         service.addJob(jobPost);
         return "success";
-
     }
 
     @GetMapping("viewalljobs")
--- src/main/java/com/app/service/JobService.java
+++ src/main/java/com/app/service/JobService.java
@@ -12,20 +12,14 @@ public class JobService {
     @Autowired
     public JobRepo repo;
 
-
     // method to add a jobPost
     public void addJob(JobPost jobPost) {
         repo.addJob(jobPost);
-
     }
-
 
     //method to return all JobPosts
     public List<JobPost> getAllJobs() {
         return repo.getAllJobs();
-
-
     }
-
 
 }
