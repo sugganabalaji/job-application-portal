7c7f9cbdf28f8151e0d4e78d84e7cebe27cda376
Balaji, Suggana <sugganabalaji@gmail.com>
2026-04-06 15:21:42 +0530

    Update package structure and project metadata

--- src/main/java/com/app/JobApplication.java
+++ src/main/java/com/app/JobApplication.java
@@ -1,4 +1,4 @@
-package com.app.JobApplication;
+package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/app/JobApplicationTests.java
+++ src/test/java/com/app/JobApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.JobApplication;
+package com.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
